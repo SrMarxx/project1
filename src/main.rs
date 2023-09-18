f52b3e6b051f3c2857c94ead3e2c815cec542e00
SrMarxx <marcosgabrieldiascardoso66@gmail.com>
2023-09-18 15:38:29 -0300

    primeira tentative de um app rocket

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
-use std::io;
+#[macro_use] extern crate rocket;
 
-fn convert_to_int(n:&String) -> u8{
-    let x = n.trim().parse::<u8>().unwrap();
-    x
+#[get("/hello/<marcos>/<21>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
 }
 
-fn main() {
-    let mut number1 = String::new();
-    let mut number2 = String::new();
-
-    println!("\nInsira o primeiro número: ");
-    io::stdin().read_line(&mut number1).expect("Erro ao ler a linha.");
-
-    println!("\nInsira o segundo número: ");
-    io::stdin().read_line(&mut number2).expect("Erro ao ler a linha.");
-
-    if convert_to_int(&number1) > convert_to_int(&number2){
-        println!("O número {} é maior que o {}", number1, number2);
-    }
-    else {
-        println!("O número {} é menor ou igual ao {}", number1, number2)
-    }
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
