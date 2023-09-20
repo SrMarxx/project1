82acede7c110f043d7ef70daf255b0cbe0d42e3a
SrMarxx <marcosgabrieldiascardoso66@gmail.com>
2023-09-20 13:57:59 -0300

    desisti do rocket

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-#[macro_use] extern crate rocket;
-
-#[get("/hello/<marcos>/<21>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+fn main() {
+    println!("Hello world");
 }
